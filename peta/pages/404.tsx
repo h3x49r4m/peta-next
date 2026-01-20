d2f238d57d036dd0a60b83ac71e700b46033be7d
h3x49r4m <h3x49r4m@outlook.com>
2026-01-20 12:48:15 -0800

    [main] fixed double headers and footers. 	modified:   peta/pages/404.tsx 	modified:   peta/pages/500.tsx

--- peta/pages/404.tsx
+++ peta/pages/404.tsx
@@ -1,13 +1,9 @@
-import Layout from '../components/Layout';
-
 export default function Custom404() {
   return (
-    <Layout>
-      <div style={{ textAlign: 'center', padding: '50px' }}>
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <a href="/">Return to Home</a>
-      </div>
-    </Layout>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <a href="/">Return to Home</a>
+    </div>
   );
 }
--- peta/pages/500.tsx
+++ peta/pages/500.tsx
@@ -1,13 +1,9 @@
-import Layout from '../components/Layout';
-
 export default function Custom500() {
   return (
-    <Layout>
-      <div style={{ textAlign: 'center', padding: '50px' }}>
-        <h1>500 - Server Error</h1>
-        <p>Something went wrong on our end. Please try again later.</p>
-        <a href="/">Return to Home</a>
-      </div>
-    </Layout>
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>500 - Server Error</h1>
+      <p>Something went wrong on our end. Please try again later.</p>
+      <a href="/">Return to Home</a>
+    </div>
   );
 }
